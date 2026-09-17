5d8ab7b206857264c2c24a29f09684f39b67a5a8
unknown <udedibiajoshua@gmail.com>
2026-09-17 16:22:30 +0100

    first commit

---
+++ capstone/Home page/src/larisa.js
@@ -0,0 +1,35 @@
+// Arithematic operators (+, -, /, *, %, ++, --, **)
+// Assigment operators (=, +=, -=, /=, *=, %=)
+// Comparism operators (>, >=, <, <=, ==, ===, !=)
+// Logocal operators (&&, ||, !)
+// Ternary operator (?)
+// let x = 1;
+// let y = 11;
+// x += 3
+
+// let hasAJob = false;
+// let hasCollateral = true;
+// console.log(hasAJob && hasCollateral ? "Qualified" : "Disqualified");
+// Note: Loose equality (==) returns true if all operands are equal while strict equality (===) return true when the operands are equal and of the same data-type
+
+// let engScore = 75;
+// let mathScore = 58;
+// let isPromoted = engScore >= 60 && mathScore >= 60;
+// console.log(isPromoted ? "Pass" : "Fail");
+// if varible time is between 0 and 12 log "Morning" to the console, if it's between 12 and 18 log "Afternoon" and log evening if it's more than 18.
+
+// let time = 20;
+// console.log(time > 1 && time < 12 ? "Morning" : time > 12 && time < 18 ? "Afternoon" : "Evening");
+
+// if (conditiion/expression) {
+// code to nbe executed if conditiion/expression is true 
+// } 
+
+let food = "rice";
+if (food === "rice") {
+    console.log("Eat")
+} else if (food === "swallow") {
+    console.log("I'll manage")
+} else {
+    console.log("Bye")
+}
